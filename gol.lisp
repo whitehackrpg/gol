0f129de3abcb7d0b5a8b436f00dfdc94f9c2001e
Christian Mehrstam <130791778+whitehackrpg@users.noreply.github.com>
2023-06-18 00:29:11 +0200

    Add files via upload

--- gol.lisp
+++ gol.lisp
@@ -1,21 +1,17 @@
 (defun dagame (oldhash)
   (blt:clear)
   (let ((newhash (make-hash-table)))
-    (maphash #'(lambda (k v) 
-		 (let ((count 0))
-		   (when (zerop v) 
-		     (setf (blt:cell-char (realpart k) (imagpart k)) #\x))
-		   (dolist (mod '(#C( 0 -1) #C(0  1) #C(-1 0) #C(1 0)
-				  #C(-1 -1) #C(1 -1) #C(-1 1) #C(1 1)))
-		     (when (zerop (gethash (+ mod k) oldhash 1)) 
-		       (incf count)))
-		   (setf (gethash k newhash)
-			 (if (or (and (zerop v) (or (= count 2) (= count 3)))
-				 (and (= v 1) (= count 3)))
-			     0
-			     1))))
-	     oldhash)
-    (blt:refresh)
+    (loop for k being the hash-keys in oldhash using (hash-value v)
+	  for count = 0 do
+	(when (zerop v) 
+	  (setf (blt:cell-char (realpart k) (imagpart k)) #\x))
+	(dolist (mod '(#C( 0 -1) #C(0  1) #C(-1 0) #C(1 0)
+		       #C(-1 -1) #C(1 -1) #C(-1 1) #C(1 1)))
+	  (when (zerop (gethash (+ mod k) oldhash 1)) 
+	    (incf count)))
+	(setf (gethash k newhash)
+	      (if (member (complex count v) '(2 3 #C(3 1))) 0 1))
+	finally (blt:refresh))
     (unless (blt:has-input-p) 
       (dagame newhash))))
 
@@ -26,7 +22,6 @@
 	      (dotimes (y dimy)
 		(let ((coord (complex x y)))
 		  (setf (gethash coord hash)
-			(if (member coord seed)
-			    0 
-			    1))))))))
+			(if (member coord seed) 0 1))))))))
+
 
